1cdce7235430f23be6eb1fcacf9af4f3facc7ab2
1toRyota <83629871+1toRyota@users.noreply.github.com>
2023-12-10 13:08:31 +0300

    first update

--- script.js
+++ script.js
@@ -10,15 +10,14 @@ function domReady(fn) {
 } 
   
 domReady(function () { 
-  
-    // If found you qr code 
+
     function onScanSuccess(decodeText, decodeResult) { 
-        alert("You Qr is : " + decodeText, decodeResult); 
+        alert(decodeText, decodeResult); 
     } 
   
     let htmlscanner = new Html5QrcodeScanner( 
         "my-qr-reader", 
-        { fps: 10, qrbos: 250 } 
+        { fps: 60, qrbos: 250 } 
     ); 
     htmlscanner.render(onScanSuccess); 
 });
